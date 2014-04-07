860413fe20a385c9f4238cc8ee4f53c70daf162b
Fredrik Lind <fredrik@editerat.se>
2014-04-07 21:27:07 +0200

    Solution working, just need to think it through now

--- lab1.hs
+++ lab1.hs
@@ -1,8 +1,27 @@
+import Data.List
 
 
 -- Remove duplicates
 rmd :: [Int] -> [Int]
 rmd [] = []
-rmd (x:xs) = x:[n | n <- rmd xs , n /= x]
+rmd (x:y) = x:[i | i <- rmd y , i /= x]
 
-main = print (rmd [1,2,1,2,3,3,3,3,2,2,2,2,1,2,3,1,1])
+kmaxsub :: Int -> [Int] -> [(Int,Int,Int)]
+kmaxsub k x = take k(reverse(sort([(v,i,j) | n <- subsequences x, 
+                                    let v = sum n
+                                        i = head(elemIndices (head n) x)+1
+                                        j = head(elemIndices (last n) x)+1,
+                                        n `isInfixOf` x])))
+
+kmaxsubunique :: Int -> [Int] -> [(Int,Int,Int)]
+kmaxsubunique k x = kmaxsub k (rmd x)
+
+
+-- Remove duplicates
+-- Get all possible subsequences
+-- With a list comprehension, select only continuous subsequences
+-- During the list comprehension, compile the tripe with sum, start index and end index
+-- Remove the first element (it's the empty list)
+-- Sort the resulting list
+-- Reverse that list
+-- Return the k first elements
