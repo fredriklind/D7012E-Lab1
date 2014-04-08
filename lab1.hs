bd738e112547db85fb49b072ed15d76a34c07742
Fredrik Lind <fredrik@editerat.se>
2014-04-08 14:51:07 +0200

    Changed comment

--- lab1.hs
+++ lab1.hs
@@ -7,7 +7,7 @@ kmaxsubunique x k = kmaxsub (rmd x) k
           kmaxsub x k = take k(                                         -- Get the first k triples
                 reverse(                                                -- Reverse the list
                     sort(                                               -- Sort the list
-                        [(v,i,j) |                                      -- Do a list comprehension on all possible subsequences
+                        [(v,i,j) |                                      -- Construct the triples
                             n <- (tails x) `union` (inits x),           -- Get all continuous subsequences
                                 let v = sum n                           -- Sum of the subsequence
                                     i = head(elemIndices (head n) x)+1  -- Find the index of the first element in the subsequence in the original list
