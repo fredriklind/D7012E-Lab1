b2dbe46f6b4fe00b60ed15bf65253321d9258ec2
Fredrik Lind <fredrik@editerat.se>
2014-04-08 14:49:32 +0200

    Removed the use of 'subsequences' function. Not using a union of inits and tails instead. Now the condition inside the list comprehension is not needed anymore.

--- lab1.hs
+++ lab1.hs
@@ -8,11 +8,11 @@ kmaxsubunique x k = kmaxsub (rmd x) k
                 reverse(                                                -- Reverse the list
                     sort(                                               -- Sort the list
                         [(v,i,j) |                                      -- Do a list comprehension on all possible subsequences
-                            n <- subsequences x,                        -- Get all subsequences
+                            n <- (tails x) `union` (inits x),           -- Get all continuous subsequences
                                 let v = sum n                           -- Sum of the subsequence
                                     i = head(elemIndices (head n) x)+1  -- Find the index of the first element in the subsequence in the original list
-                                    j = head(elemIndices (last n) x)+1, -- Find the index of the last element in the subsequence in the original list
-                            n `isInfixOf` x]                            -- Only include continuous subsequences
+                                    j = head(elemIndices (last n) x)+1  -- Find the index of the last element in the subsequence in the original list
+                                ]
                             )
                     )
                 )
